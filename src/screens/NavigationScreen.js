ddda35ffa1ee8d73229f2c891d48b2a6e686a0af
Vladyslav Tertyshnyi <vladyslav.tertyshnyi@navico.com>
2018-09-11 17:01:11 +0300

    Fix formatting

--- src/screens/NavigationScreen.js
+++ src/screens/NavigationScreen.js
@@ -1,10 +1,10 @@
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 import Navigation from '../components/Navigation';
 import { getAuth } from '../store/selectors/auth';
 
 const mapStateToProps = state => ({
-    auth: getAuth(state),
+  auth: getAuth(state),
 });
 
 export default connect(mapStateToProps)(Navigation);
